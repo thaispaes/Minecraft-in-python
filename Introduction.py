e6d773642cfed4b7bc2944f4540748b15d225e6f
thaispaes <thaisreginapaesdossantos@gmail.com>
2022-07-22 20:53:58 -0300

    feat: criando um construtor

--- Introduction.py
+++ Introduction.py
@@ -1,5 +1,13 @@
-
 from ursina import *
+
+class Test_cube(Entity):
+    def _init_(self):
+        super()._init_(
+            model = 'cube',
+            color = color.white,
+            texture = 'white_cube',
+            rotation = Vec3(45,45,45)
+            )
 
 def update():
     if held_keys['a']:
@@ -10,6 +18,8 @@ app = Ursina()
 test_square = Entity(model = 'quad', color = color.black, scale = (1,4), position = (5,4))
 
 sans_texture = load_texture('assets/Sans.jpeg')
-sans = Entity(model = 'quad', texture = sans_texture, scale = (4,4))
+sans = Entity(model = 'quad', texture = sans_texture)
+
+test_cube = Test_cube()
 
 app.run()
