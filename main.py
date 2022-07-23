29c58f7853c434933589b0f6ddd2f6898e73593a
thaispaes <thaisreginapaesdossantos@gmail.com>
2022-07-22 22:38:09 -0300

    feat: blocos criados

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
 from ursina import * 
 from ursina.prefabs.first_person_controller import FirstPersonController
 
+app = Ursina()
+
+grass_texture = load_texture('assets/grass_block.png')
+stone_texture = load_texture('assets/stone_block.png')
+brick_texture = load_texture('assets/brick_block.png')
+dirt_texture = load_texture('assets/dirt_block.png')
+block_pick = 1
+
+def update():
+    global block_pick
+    if held_keys['1']: block_pick = 1
+    if held_keys['2']: block_pick = 2
+    if held_keys['3']: block_pick = 3
+    if held_keys['4']: block_pick = 4
+
 class Voxel(Button):
-    def __init__(self, position = (0,0,0)):
+    def __init__(self, position = (0,0,0), texture = grass_texture):
         super().__init__(
             parent = scene,
             position = position,
-            model = 'cube',
-            origin_y = 1.2,
-            texture = 'white_cube',
+            model = 'assets/block',
+            origin_y = 0.5,
+            texture = texture,
             color = color.color(0,0,random.uniform(0.9,1)),
-            highlight_color = color.lime)
+            highlight_color = color.lime,
+            scale = 0.5)
 
     def input(self,key):
         if self.hovered: 
             if key == 'left mouse down':
-                voxel = Voxel(position = self.position + mouse.normal)
+                if block_pick == 1:
+                    voxel = Voxel(position = self.position + mouse.normal, texture = grass_texture)
+                if block_pick == 2:
+                    voxel = Voxel(position = self.position + mouse.normal, texture = stone_texture)
+                if block_pick == 3:
+                    voxel = Voxel(position = self.position + mouse.normal, texture = brick_texture)
+                if block_pick == 4:
+                    voxel = Voxel(position = self.position + mouse.normal, texture = dirt_texture)
 
             if key == 'right mouse down':
                 destroy(self)
-
-app = Ursina()
 
 for z in range(20):
     for x in range(20):
