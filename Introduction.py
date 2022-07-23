4d4755efabddbfc0ef0c4bd379c79d291e3833a9
thaispaes <thaisreginapaesdossantos@gmail.com>
2022-07-22 21:10:47 -0300

    add: assets do projeto

--- Introduction.py
+++ Introduction.py
@@ -9,6 +9,14 @@ class Test_cube(Entity):
             rotation = Vec3(45,45,45)
             )
 
+class Test_button(Button):
+    def _init_(self):
+        super()._init_(
+            model= 'cube',
+            texture = 'brick',
+            color = color.blue
+        )
+
 def update():
     if held_keys['a']:
         test_square.x -=4 * time.dt
@@ -17,7 +25,7 @@ app = Ursina()
 
 test_square = Entity(model = 'quad', color = color.black, scale = (1,4), position = (5,4))
 
-sans_texture = load_texture('assets/Sans.jpeg')
+sans_texture = load_texture('assets/Sans')
 sans = Entity(model = 'quad', texture = sans_texture)
 
 test_cube = Test_cube()
