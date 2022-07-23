f264b83f2cb0a5cc1e3b92e2dbe023526c0d2cde
thaispaes <thaisreginapaesdossantos@gmail.com>
2022-07-22 22:26:39 -0300

    feat: blocks criados

--- main.py
+++ main.py
@@ -7,15 +7,23 @@ class Voxel(Button):
             parent = scene,
             position = position,
             model = 'cube',
-            origin_y = 0.5,
+            origin_y = 1.2,
             texture = 'white_cube',
-            color = color.white,
+            color = color.color(0,0,random.uniform(0.9,1)),
             highlight_color = color.lime)
+
+    def input(self,key):
+        if self.hovered: 
+            if key == 'left mouse down':
+                voxel = Voxel(position = self.position + mouse.normal)
+
+            if key == 'right mouse down':
+                destroy(self)
 
 app = Ursina()
 
-for z in range(8):
-    for x in range(8):
+for z in range(20):
+    for x in range(20):
         voxel = Voxel(position = (x,0,z))
 
 player = FirstPersonController()
